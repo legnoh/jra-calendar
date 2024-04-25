10d45dc598f316c94eeb956d3e315519f1b42983
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-04-25 22:53:10 +0900

    add overseas logics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime,json,logging,os
 import modules.jra as jra
 import modules.nar as nar
+import modules.overseas as overseas
 import modules.ical as jraIcal
 import modules.json as jraJson
 from selenium.webdriver.chrome.service import Service
@@ -20,34 +21,48 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(service=Service(), options=options)
     driver.implicitly_wait(10)
 
-    logging.info("# JRA")
-
-    ## get active link point settings
-    logging.info("## Get active link point settings")
-    max_link_point = jra.get_max_link_point()
-    
-    ## get all active year
-    logging.info("## Get all active year")
-    years:list[str] = jra.get_calendar_active_years(driver)
-
-    ## get all JRA grade race in all active year
-    logging.info("## Get all JRA grade race in all active year")
     jra_grade_races = []
-    for year in years:
-        for month in range(1, 13):
-            jra_grade_races = jra_grade_races + jra.get_grade_races_by_month(driver, year, month, max_link_point)
+    dirt_grade_races = []
+    overseas_grade_races = []
+
+    # logging.info("# JRA")
+
+    # ## get active link point settings
+    # logging.info("## Get active link point settings")
+    # max_link_point = jra.get_max_link_point()
     
-    logging.info("# NAR")
+    # ## get all active year
+    # logging.info("## Get all active year")
+    # years:list[str] = jra.get_calendar_active_years(driver)
+
+    # ## get all JRA grade race in all active year
+    # logging.info("## Get all JRA grade race in all active year")
+    
+    # for year in years:
+    #     for month in range(1, 13):
+    #         jra_grade_races = jra_grade_races + jra.get_grade_races_by_month(driver, year, month, max_link_point)
+    
+    # logging.info("# NAR")
+
+    # ## get all active year
+    # logging.info("## Get all active year")
+    # years:list[str] = nar.get_calendar_active_years(driver)
+
+    # ## get all NAR grade race in all active year
+    # logging.info("## Get all Dirt grade race in all active year")
+    # for year in years:
+    #     dirt_grade_races = dirt_grade_races + nar.get_grade_races_by_year(driver, year)
+    
+    logging.info("# OVERSEAS")
 
     ## get all active year
     logging.info("## Get all active year")
-    years:list[str] = nar.get_calendar_active_years(driver)
+    years:list[str] = overseas.get_calendar_active_years(driver)
 
     ## get all NAR grade race in all active year
-    logging.info("## Get all Dirt grade race in all active year")
-    dirt_grade_races = []
+    logging.info("## Get all Overseas grade race in all active year")
     for year in years:
-        dirt_grade_races = dirt_grade_races + nar.get_grade_races_by_year(driver, year)
+        overseas_grade_races = overseas_grade_races + overseas.get_grade_races_by_year(driver, year)
 
     # # compose iCalendar file
     logging.info("# Generate ical data")
@@ -58,6 +73,9 @@ if __name__ == '__main__':
         event = jraIcal.create_event_block(race)
         cal.add_component(event)
     for race in dirt_grade_races:
+        event = jraIcal.create_event_block(race)
+        cal.add_component(event)
+    for race in overseas_grade_races:
         event = jraIcal.create_event_block(race)
         cal.add_component(event)
     
@@ -75,6 +93,13 @@ if __name__ == '__main__':
         event = jraIcal.create_event_block(race)
         cal_nar.add_component(event)
 
+    cal_overseas = Calendar()
+    cal_overseas.add("X-WR-CALNAME", "競馬重賞(海外)")
+    cal_overseas.add("X-APPLE-CALENDAR-COLOR", "#0069A7")
+    for race in overseas_grade_races:
+        event = jraIcal.create_event_block(race)
+        cal_overseas.add_component(event)
+
     logging.info("# Output ics file")
     if not os.path.exists("./dist"):
         os.mkdir("./dist")
@@ -84,6 +109,8 @@ if __name__ == '__main__':
         f.write(cal_jra.to_ical().decode("utf-8"))
     with open("./dist/graderaces_dirtgrade.ics", mode='w') as f:
         f.write(cal_nar.to_ical().decode("utf-8"))
+    with open("./dist/graderaces_overseas.ics", mode='w') as f:
+        f.write(cal_overseas.to_ical().decode("utf-8"))
 
     logging.info("# Output json file")
     now = datetime.datetime.now()
@@ -96,6 +123,9 @@ if __name__ == '__main__':
         json.dump(d, f, indent=2, default=jraJson.json_serial)
     with open("./dist/graderaces_dirtgrade.json", mode='w') as f:
         d = { "updated_at": now, "races": dirt_grade_races }
+        json.dump(d, f, indent=2, default=jraJson.json_serial)
+    with open("./dist/graderaces_overseas.json", mode='w') as f:
+        d = { "updated_at": now, "races": overseas_grade_races }
         json.dump(d, f, indent=2, default=jraJson.json_serial)
 
     logging.info("All process was done successfully.")
