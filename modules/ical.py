c99c154ca546b6a284f4cad4df515410b738b529
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-03-01 15:42:57 +0900

    tiny fix

--- modules/ical.py
+++ modules/ical.py
@@ -489,7 +489,7 @@ def create_event_block(race: dict):
     uid_enc = raw_uid.encode('utf-8')
     uid = base64.b64encode(uid_enc)
 
-    # タイトルを作る（G1/G2/G3の部分はローマ数字に書き換える）
+    # タイトルを作る（グレード表記の部分は絵文字に書き換える）
     summary = f"{race['grade'].replace("G1","🥇").replace("G2","🥈").replace("G3","🥉").replace("Jpn1","🥇").replace("Jpn2","🥈").replace("Jpn3","🥉")}{race['name']}"
 
     # 競馬場の情報を取得する(X-APPLE-STRUCTURED_LOCATIONの形式に合わせる)
